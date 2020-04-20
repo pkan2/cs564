fc634404c02ad4cc4c9c5372cde113e02c84ba53
pkan2 <pkan2@wisc.edu>
2020-04-20 14:01:57 -0500

    update btree cpp index method

--- project3/Btree/src/btree.cpp
+++ project3/Btree/src/btree.cpp
@@ -32,7 +32,101 @@ BTreeIndex::BTreeIndex(const std::string & relationName,
 		const int attrByteOffset,
 		const Datatype attrType)
 {
-
+    this -> bufMgr = bufMgrIn;
+    this -> attributeType = attrType;
+    this -> attrByteOffset = attrByteOffset;
+    // update the value of node occupancy and leaf node occupancy
+    // this part of code is following the variable INTARRAYLEAFSIZE and
+    // INTARRAYNONLEAFSIZE from the btree.h
+    switch(attrType){
+        case INTEGER:
+            this -> nodeOccupancy = (Page::SIZE - sizeof(int) - sizeof(PageId)) / (sizeof(int) + sizeof(PageId));
+            this -> leafOccupancy = (Page::SIZE - sizeof(PageId)) / (sizeof(int) + sizeof(RecordId));
+            break;
+        case DOUBLE:
+            this -> nodeOccupancy = (Page::SIZE - sizeof(double) - sizeof(PageId)) / (sizeof(double) + sizeof(PageId));
+            this -> leafOccupancy = (Page::SIZE - sizeof(PageId)) / (sizeof(double) + sizeof(RecordId));
+            break;
+        case STRING:
+            // the size of string record is provided in the instruction file
+            this -> nodeOccupancy = (Page::SIZE - sizeof(char[64]) - sizeof(PageId)) / (sizeof(char[64]) + sizeof(PageId));
+            this -> leafOccupancy = (Page::SIZE - sizeof(PageId)) / (sizeof(char[64]) + sizeof(RecordId));
+            break;
+    }
+    
+    this -> scanExecuting = false;
+    std::ostringstream idxStr;
+    idxStr << relationName << '.' << attrByteOffset;
+    // indexName is the name of the index file
+    std::string indexName = idxStr.str();
+    outIndexName = indexName;
+    
+    // check if the index file is already existing or not
+    if(File::exists(indexName)){
+        BlobFile file =  BlobFile::open(indexName);
+        // check whether the existing file matching with our input info
+        // find the meta page
+        PageId metaPageId = file.getFirstPageNo();
+        Page * metaPage;
+        this -> bufMgr -> readPage(&file, metaPageId, metaPage);
+        IndexMetaInfo * metaInfo = (IndexMetaInfo *) metaPage;
+        // check the attribute in the meta page
+        if(!(metaInfo -> relationName == relationName
+           && metaInfo -> attrByteOffset == attrByteOffset
+           && metaInfo -> attrType == attrType)){
+            this -> bufMgr -> unPinPage(&file, metaPageId, false);
+            throw BadIndexInfoException("Index file exists but values in metapage not match.");
+        }
+        this -> headerPageNum = metaPageId;
+        this -> rootPageNum = metaInfo -> rootPageNo;
+        this -> file = (&file);
+        this -> bufMgr -> unPinPage(&file, metaPageId, false);
+        return;
+    }
+    
+    BlobFile file = BlobFile::create(indexName);
+    this -> file = & file;
+    // create the metadata page
+    PageId metaPageId;
+    Page * metaPage;
+    this -> bufMgr -> allocPage(&file, metaPageId, metaPage);
+    IndexMetaInfo * metaInfo = (IndexMetaInfo *) metaPage;
+    // write the input attributes into the metaPage
+    strcpy(metaInfo -> relationName, relationName.c_str());
+    metaInfo -> attrByteOffset = attrByteOffset;
+    metaInfo -> attrType = attrType;
+    // assign the meta page id to the private attribute
+    this -> headerPageNum = metaPageId;
+    // create the root page and update the attribute for root page
+    // update the root page attribute in the metaPage
+    PageId rootPageId;
+    Page * rootPage;
+    this -> bufMgr -> allocPage(&file, rootPageId, rootPage);
+    this -> bufMgr -> unPinPage(&file, rootPageId, true);
+    metaInfo -> rootPageNo = rootPageId;
+    this -> rootPageNum = rootPageId;
+    this -> bufMgr -> unPinPage(&file, metaPageId, true);
+    // scan the relation and insert into the index file
+    FileScan fileScan(relationName, bufMgrIn);
+    try
+    {
+        RecordId scanRid;
+        while(1)
+        {
+            fileScan.scanNext(scanRid);
+            std::string recordStr = fileScan.getRecord();
+            const char *record = recordStr.c_str();
+            // as mentioned in the instruction, the data type of key
+            // in this assignment will only be integer.
+            int key = *((int *)(record + attrByteOffset));
+            this -> insertEntry(&key, scanRid);
+        }
+    }
+    catch(EndOfFileException e)
+    {
+        // this case means reaching the end of the relation file.
+        //std::cout << "Read all records" << std::endl;
+    }
 }
 
 
